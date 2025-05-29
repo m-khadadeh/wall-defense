6915df9ef5df8f24b71dd9b98e559f36241ee0c0
Victoria Lim <36555678+vslim@users.noreply.github.com>
2025-05-29 01:13:10 -0700

    Main Scene Setup

---
+++ Assets/01_Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace WallDefense
+{
+    public class GameManager : MonoBehaviour
+    {
+        public WallManager wallManager;
+        public DayNightManager dayNightManager;
+        public SaveManager saveManager;
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
