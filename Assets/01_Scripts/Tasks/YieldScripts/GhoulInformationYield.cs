bfbdc8d8f40b0c1ed587a9156ae007fa44797570
m-khadadeh <mkhudadeh@gmail.com>
2025-06-08 05:49:05 +0300

    Fixed clue sign report

--- Assets/01_Scripts/Tasks/YieldScripts/GhoulInformationYield.cs
+++ Assets/01_Scripts/Tasks/YieldScripts/GhoulInformationYield.cs
@@ -11,7 +11,7 @@ namespace WallDefense
     public override void GetYield(ColonyData colony, string choice, List<ItemType> consumed)
     {
       Clue clue = _selector.FindClue();
-      string innerData = (clue == null) ? clue.dialogBoxInfo : "Nothing of note remaining";
+      string innerData = (clue != null) ? clue.dialogBoxInfo : "Nothing of note remaining";
       string clueData = "<indent=0%><align=\"center\">-------------------------------------\n";
       clueData += "<size=40>INVESTIGATION REPORT</size>\n";
       clueData += "-------------------------------------\n";
